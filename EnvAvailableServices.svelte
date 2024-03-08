fed4c9d8e454c95c1393ca270471b8896fce2806
abhaybhatia-fareye <160734130+abhaybhatia-fareye@users.noreply.github.com>
2024-03-08 12:28:35 +0530

    Update EnvAvailableServices.svelte
    added mobi

--- EnvAvailableServices.svelte
+++ EnvAvailableServices.svelte
@@ -1,18 +1,54 @@
-<style>
-    .hover {
-        transition: all 300ms;
-    }
 
-    .hover:hover {
-        width: 23%; /* Adjust the width on hover */
-        height: 5%; /* Adjust the height on hover */
-    }
-</style>
+
+<div class="flex justify-evenly">
+
+
+    
+
+
+
+
+
+
+    <div class="flex justify-center mt-[13%] ">
+        <div></div>
+        <div
+            class="w-[15vw] hover:shadow-2xl shadow-md shadow-slate-800 hover:shadow-black-500 hover flex justify-center flex-col bg-gray-200 rounded-md border-2 border-black"
+        >
+            <div class="">
+                <div
+                    class="py-1 flex justify-center text-2xl font-bold text-black font-serif"
+                >
+                    MOBI
+                </div>
+                <div class="py-1 flex justify-center text-xl text-zinc-500">
+                    v-1.2.2
+                </div>
+            </div>
+            
+        </div>
+    
+    
+    
+    
+    
+    
+    
+        
+    </div>
+
+
+
+
+    
+
+
+
 
 <div class="flex justify-center mt-[13%] ">
     <div></div>
     <div
-        class="w-[22vw] shadow-lg shadow-slate-800 hover:shadow-red-500 hover:w-[23%] hover flex justify-center flex-col bg-gray-200 rounded-md border-2 border-black"
+        class="w-[22vw] hover:shadow-2xl shadow-md shadow-slate-800 hover:shadow-black-500  hover flex justify-center flex-col bg-gray-200 rounded-md border-2 border-black"
     >
         <div class="">
             <div
@@ -27,9 +63,9 @@
         <hr />
         <div class="flex justify-center">
             <div
-                class="flex justify-center flex-col bg-zinc-600 text-white m-3 p-3 rounded-md hover:w-[42%] hover:shadow-lg hover:shadow-blue-300"
+                class="flex justify-center flex-col bg-zinc-600 text-white m-3 hover:shadow-lg hover:shadow-blue-200 p-3 rounded-md "
             >
-                <div class="head flex justify-center font-bold text-blue-50">
+                <div class="head flex justify-center font-bold text-blue-50 ">
                     TRIP CONSUMER
                 </div>
                 <div class=" flex justify-center font-thin text-blue-50">
@@ -38,7 +74,7 @@
             </div>
 
             <div
-                class="flex justify-center flex-col bg-zinc-600 text-white hover:shadow-lg hover:shadow-blue-300 m-3 p-3 rounded-md hover:w-[42%]"
+                class="flex justify-center flex-col bg-zinc-600 text-white hover:shadow-lg hover:shadow-blue-200 m-3 p-3 rounded-md "
             >
                 <div class="head flex justify-center font-bold text-blue-50">
                     TRIP CONSUMER
@@ -47,6 +83,9 @@
                     v-1.2.2
                 </div>
             </div>
+            
+            <br>
+            
         </div>
     </div>
 
@@ -57,4 +96,5 @@
 
 
     
+</div>
 </div>
