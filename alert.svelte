ceb7cb50161fa31cdd8db142760d836b5d03d96d
abhaybhatia-fareye <160734130+abhaybhatia-fareye@users.noreply.github.com>
2024-03-08 14:54:11 +0530

    Update alert.svelte
    bdhb

--- alert.svelte
+++ alert.svelte
@@ -1,10 +1,31 @@
-<div class="lg:w-[30%] md:w-[20%] w-[80vw] rounded-xl lg:m-7 m-5 lg:h-[20vh] h-[25vh] align-middle mt-[103px] mx-auto border-2 border-black bg-slate-200 p-8">
-    <div class="text-xl">Alert</div>
+<div class="flex justify-center cursor-pointer">
+<div class="aa lg:w-fit md:w-fit w-[80vw] shadow-2xl shadow-gray-300 hover:shadow-gray-400 rounded-xl lg:m-7 m-5 lg:h-[20vh] h-[25vh] align-middle mt-[103px] mx-auto border-2 border-gray-400 bg-white p-8">
+    <div class="flex">
+        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 mb-1 m-2" viewBox="0 0 256 257"><path fill="#f60" d="M245.734 102.437h-81.911a10.199 10.199 0 0 1-10.263-10.263v-81.91a10.199 10.199 0 0 0-10.263-10.2h-30.66a10.199 10.199 0 0 0-10.264 10.2v81.91a10.199 10.199 0 0 1-10.263 10.263H61.45a10.199 10.199 0 0 1-10.263-10.263v-81.91A10.199 10.199 0 0 0 40.924 0H10.199A10.2 10.2 0 0 0 0 10.263v235.535a10.2 10.2 0 0 0 10.263 10.263h235.47a10.2 10.2 0 0 0 10.264-10.263V112.893a10.199 10.199 0 0 0-10.263-10.456m-41.18 86.979a15.331 15.331 0 0 1-15.33 15.394h-20.526a15.33 15.33 0 0 1-15.33-15.394v-20.462a15.331 15.331 0 0 1 15.33-15.394h20.525a15.33 15.33 0 0 1 15.33 15.394z"/></svg>
+        <div class="text-3xl font-sans  mx-1">RABBITMQ</div>
+    </div>
     <hr class="w-full h-1 bg-black">
-    <div class="p-3  border-2 border-grey-700 w-[100%] h-[30%] flex">
-        <div class="p-3">Alert 1</div>
-        <div class="p-3">Alert 2</div>
-        <div class="p-3">Alert 3</div>
+    <div class="p-3   w-[100%] h-[30%] flex justify-evenly">
+        <div class="aa p-3 w-fit hover:shadow-lg h-fit m-3 rounded-md bg-gray-100 hover:bg-gray-200 border-gray-400 font-serif border-2">Alert 1</div>
+        <div class="aa p-3 w-fit hover:shadow-lg h-fit m-3 rounded-md bg-gray-100 hover:bg-gray-200 border-gray-400 font-serif border-2">Alert 2</div>
+        <div class="aa p-3 w-fit hover:shadow-lg h-fit m-3 rounded-md bg-gray-100 hover:bg-gray-200 border-gray-400 font-serif border-2">Alert 3</div>
         
     </div>
+</div>
+
+
+
+<div class="aa lg:w-fit md:w-fit w-[80vw] shadow-2xl shadow-gray-300 hover:shadow-gray-400 rounded-xl lg:m-7 m-5 lg:h-[20vh] h-[25vh] align-middle mt-[103px] mx-auto border-2 border-gray-400 bg-white p-8">
+    <div class="flex">
+        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 mb-1 m-2" viewBox="0 0 24 24"><path fill="#051fe6" d="M4 21h16l-10 3zm0-1l6-1V8zm7-1l9 1l-9-20z"/></svg> 
+        <div class="text-3xl font-sans  mx-1">ISTIO</div>
+    </div>
+    <hr class="w-full h-1 bg-black">
+    <div class="p-3   w-[100%] h-[30%] flex justify-evenly">
+        <div class="aa p-3 w-fit hover:shadow-lg h-fit m-3 rounded-md bg-gray-100 hover:bg-gray-200 border-gray-400 font-serif border-2">Alert 1</div>
+        <div class="aa p-3 w-fit hover:shadow-lg h-fit m-3 rounded-md bg-gray-100 hover:bg-gray-200 border-gray-400 font-serif border-2">Alert 2</div>
+        <div class="aa p-3 w-fit hover:shadow-lg h-fit m-3 rounded-md bg-gray-100 hover:bg-gray-200 border-gray-400 font-serif border-2">Alert 3</div>
+        
+    </div>
+</div>
 </div>
