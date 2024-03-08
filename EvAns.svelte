44840303fdcfeba537a95433e9b801fd06fd65a1
abhaybhatia-fareye <160734130+abhaybhatia-fareye@users.noreply.github.com>
2024-03-08 12:07:49 +0530

    Update EvAns.svelte
    changed bg color

--- EvAns.svelte
+++ EvAns.svelte
@@ -12,7 +12,7 @@
 </style>
 
 <div class="flex flex-col justify-center items-center lg:mx-auto mx-2 lg:mt-32">
-    <div class="p-4 bg-gray-200 lg:w-[37%] hover:border-gray-600 backdrop-blur-sm hover:backdrop-blur-lg hover:border-2 shadow-lg  hover:shadow-2xl font-serif font-bold w-[95%] hover:w rounded-sm flex justify-center hover:bg-gray-300 my-3 hover:text-black text-black hover:w-[39%] hover:h-[5%] hover">VERSION FE 12.2</div>
-    <div class="p-4 bg-gray-200 lg:w-[37%] hover:border-gray-600 backdrop-blur-sm hover:backdrop-blur-lg hover:border-2 shadow-lg  hover:shadow-2xl font-serif font-bold w-[95%] hover:w rounded-sm flex justify-center hover:bg-gray-300 my-3 hover:text-black text-black hover:w-[39%] hover:h-[5%] hover">HTTPS://GITHUB.COM</div>
-    <div class="p-4 bg-gray-200 lg:w-[37%] hover:border-gray-600 backdrop-blur-sm hover:backdrop-blur-lg hover:border-2 shadow-lg  hover:shadow-2xl font-serif font-bold w-[95%] hover:w rounded-sm flex justify-center hover:bg-gray-300 my-3 hover:text-black text-black hover:w-[39%] hover:h-[5%] hover">30PJHBFHBFJKEFNJWEFJKNJKWFJKN</div>
+    <div class="p-4 bg-white lg:w-[37%] hover:border-gray-300 backdrop-blur-sm hover:backdrop-blur-lg hover:border-2 shadow-lg  hover:shadow-2xl font-serif font-bold w-[95%] hover:w rounded-sm flex justify-center hover:bg-gray-600 my-3 hover:text-black text-black hover:w-[39%] hover:h-[5%] hover">VERSION FE 12.2</div>
+    <div class="p-4 bg-white lg:w-[37%] hover:border-gray-300 backdrop-blur-sm hover:backdrop-blur-lg hover:border-2 shadow-lg  hover:shadow-2xl font-serif font-bold w-[95%] hover:w rounded-sm flex justify-center hover:bg-gray-600 my-3 hover:text-black text-black hover:w-[39%] hover:h-[5%] hover">HTTPS://GITHUB.COM</div>
+    <div class="p-4 bg-white lg:w-[37%] hover:border-gray-300 backdrop-blur-sm hover:backdrop-blur-lg hover:border-2 shadow-lg  hover:shadow-2xl font-serif font-bold w-[95%] hover:w rounded-sm flex justify-center hover:bg-gray-600 my-3 hover:text-black text-black hover:w-[39%] hover:h-[5%] hover">30PJHBFHBFJKEFNJWEFJKNJKWFJKN</div>
 </div>
