a1c52e78a8bca2b042bdef15d22055c85300270e
abhaybhatia-fareye <160734130+abhaybhatia-fareye@users.noreply.github.com>
2024-03-08 12:41:53 +0530

    Update EnvAvailableServices.svelte
    used only tailwind css

--- EnvAvailableServices.svelte
+++ EnvAvailableServices.svelte
@@ -1,19 +1,7 @@
-
-
 <div class="flex justify-evenly">
-
-
-    
-
-
-
-
-
-
     <div class="flex justify-center mt-[13%] ">
-        <div></div>
         <div
-            class="w-[15vw] hover:shadow-2xl shadow-md shadow-slate-800 hover:shadow-black-500 hover flex justify-center flex-col bg-gray-200 rounded-md border-2 border-black"
+            class="w-[15vw] aa hover:w-[20vw] hover:shadow-2xl shadow-md shadow-slate-800 max-w-sm hover:max-w-lg hover:shadow-black-500 hover flex justify-center flex-col bg-gray-200 rounded-md border-2 border-black transition-width duration-1000"
         >
             <div class="">
                 <div
@@ -25,76 +13,47 @@
                     v-1.2.2
                 </div>
             </div>
-            
-        </div>
-    
-    
-    
-    
-    
-    
-    
-        
-    </div>
-
-
-
-
-    
-
-
-
-
-<div class="flex justify-center mt-[13%] ">
-    <div></div>
-    <div
-        class="w-[22vw] hover:shadow-2xl shadow-md shadow-slate-800 hover:shadow-black-500  hover flex justify-center flex-col bg-gray-200 rounded-md border-2 border-black"
-    >
-        <div class="">
-            <div
-                class="py-1 flex justify-center text-2xl font-bold text-black font-serif"
-            >
-                TRIP
-            </div>
-            <div class="py-1 flex justify-center text-xl text-zinc-500">
-                v-1.2.2
-            </div>
-        </div>
-        <hr />
-        <div class="flex justify-center">
-            <div
-                class="flex justify-center flex-col bg-zinc-600 text-white m-3 hover:shadow-lg hover:shadow-blue-200 p-3 rounded-md "
-            >
-                <div class="head flex justify-center font-bold text-blue-50 ">
-                    TRIP CONSUMER
-                </div>
-                <div class=" flex justify-center font-thin text-blue-50">
-                    v-1.2.2
-                </div>
-            </div>
-
-            <div
-                class="flex justify-center flex-col bg-zinc-600 text-white hover:shadow-lg hover:shadow-blue-200 m-3 p-3 rounded-md "
-            >
-                <div class="head flex justify-center font-bold text-blue-50">
-                    TRIP CONSUMER
-                </div>
-                <div class=" flex justify-center font-thin text-blue-50">
-                    v-1.2.2
-                </div>
-            </div>
-            
-            <br>
-            
         </div>
     </div>
 
+    <div class="flex justify-center mt-[13%] ">
+        <div
+            class="w-[22vw] aa hover:w-[27.5vw] hover:shadow-2xl shadow-md shadow-slate-800 hover:shadow-black-500 hover flex justify-center flex-col bg-gray-200 rounded-md border-2 border-black transition-width duration-1000"
+        >
+            <div class="">
+                <div
+                    class="py-1 flex justify-center text-2xl font-bold text-black font-serif"
+                >
+                    TRIP
+                </div>
+                <div class="py-1 flex justify-center text-xl text-zinc-500">
+                    v-1.2.2
+                </div>
+            </div>
+            <hr />
+            <div class="flex justify-center">
+                <div
+                    class="flex justify-center flex-col bg-zinc-600 text-white m-3 hover:shadow-lg hover:shadow-blue-200 p-3 rounded-md "
+                >
+                    <div class="head flex justify-center font-bold text-blue-50 ">
+                        TRIP CONSUMER
+                    </div>
+                    <div class=" flex justify-center font-thin text-blue-50">
+                        v-1.2.2
+                    </div>
+                </div>
 
-
-
-
-
-
-    
-</div>
+                <div
+                    class="flex justify-center flex-col bg-zinc-600 text-white hover:shadow-lg hover:shadow-blue-200 m-3 p-3 rounded-md "
+                >
+                    <div class="head flex justify-center font-bold text-blue-50">
+                        TRIP CONSUMER
+                    </div>
+                    <div class=" flex justify-center font-thin text-blue-50">
+                        v-1.2.2
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
